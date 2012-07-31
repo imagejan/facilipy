08c8f9279cd5593e449956968188c947b60f77c9
Jan Eglinger <jan.eglinger@github>
2012-07-31 13:54:00 +0200

    bookings: added Models for Resource and Booking
    - added Resource model
    - added Booking model
    - installed bokkings app
    - registered models with admin

---
+++ bookings/admin.py
@@ -0,0 +1,5 @@
+from bookings.models import Resource, Booking
+from django.contrib import admin
+
+admin.site.register(Resource)
+admin.site.register(Booking)
--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Resource(models.Model):
+	name = models.CharField(max_length=200)
+
+class Booking(models.Model):
+	user = models.ForeignKey(User)
+	resource = models.ForeignKey(Resource)
+	blackout = models.BooleanField(default=False)
+	start = models.DateTimeField('start date and time')
+	end = models.DateTimeField('end date and time')
